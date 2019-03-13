16d0f27e05c9ffc34075f10cf4521afe6f82290d
DavidLBallard <email_id>
2019-03-13 17:20:43 -0500

    completed with email set up

--- Not_working_site.py
+++ Not_working_site.py
@@ -1,6 +1,26 @@
-
+# import statements
+# smtplib allows sending of an email
+# requests ping's website
+import smtplib
 import requests
-
-r = requests.get('', timeout=5)
+# global variables that for the email address and password
+EMAIL_ADDRESS = ""
+EMAIL_PASSWORD = ""
+website = ""
+# creates an instance of requests with the get method searching for a
+# response from the given website
+# (Current website is blank)
+r = requests.get(website, timeout=5)
 
 if r.status_code != 200:
+    with smtplib.SMTP('#input email server',   "#input port number as a number") as smtp:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.ehlo()
+
+        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+
+        subject = 'The site is currently down'
+        body = 'The ' + website + " is currently down and needing attention."
+        msg = f'Subject: {subject}\n\n{body}'
+        smtp.sendmail(EMAIL_ADDRESS, "# input a receiver email", msg)
